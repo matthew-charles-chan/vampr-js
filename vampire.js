daaffb047c706d3fdd8fa8a81af2267cd6882b83
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-01-24 21:56:04 +0000

    implementing tree logic

--- vampire.js
+++ vampire.js
@@ -4,28 +4,39 @@ class Vampire {
     this.yearConverted = yearConverted;
     this.offspring = [];
     this.creator = null;
+
+
+
   }
 
   /** Simple tree methods **/
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVamp = this;
 
+    while (currentVamp.creator) {
+      currentVamp = currentVamp.creator;
+      numberOfVampires ++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal);
   }
 
   /** Stretch **/
@@ -36,7 +47,27 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
+    let currentVamp = this;
+    if (currentVamp.creator === null) {
+      return currentVamp;
+    } else if (vampire.creator === null) {
+      return vampire.creator;
+    } else if (vampire.name === currentVamp.name) {
+      return currentVamp;
+    }
+    if (vampire === this.creator) {
+      return vampire;
+    } else if (this === vampire.creator) {
+      return this;
+    }
+    while (currentVamp.creator !== vampire.creator) {
+      if (currentVamp.isMoreSeniorThan(vampire)) {
+        vampire = vampire.creator;
+      } else {
+        currentVamp = currentVamp.creator;
+      }
+    }
+    return currentVamp.creator;
   }
 }
 
